022ea6cb530163538fb775cbe717abee6a448380
Chris Hayduk <chayduk@fordham.edu>
2026-02-11 01:20:51 -0500

    Add MSA Column Attention and MSA Transition

--- minalphafold/evoformer.py
+++ minalphafold/evoformer.py
@@ -66,6 +66,7 @@ class MSARowAttentionWithPairBias(torch.nn.Module):
 
         attention = torch.nn.functional.softmax(scores, dim=-1)
 
+        # Shape (batch, N_seq, N_res, self.num_heads, self.head_dim)
         values = torch.einsum('bshij, bsjhd -> bsihd', attention, V)
 
         values = G * values
@@ -79,18 +80,74 @@ class MSARowAttentionWithPairBias(torch.nn.Module):
 class MSAColumnAttention(torch.nn.Module):
     def __init__(self, config):
         super().__init__()
-        pass
+        self.layer_norm_msa = torch.nn.LayerNorm(config.c_s)
+
+        self.head_dim = config.dim
+        self.num_heads = config.num_heads
+
+        self.total_dim = self.head_dim * self.num_heads
+
+        self.linear_q = torch.nn.Linear(in_features=config.c_s, out_features=self.total_dim, bias=False)
+        self.linear_k = torch.nn.Linear(in_features=config.c_s, out_features=self.total_dim, bias=False)
+        self.linear_v = torch.nn.Linear(in_features=config.c_s, out_features=self.total_dim, bias=False)
+
+        self.linear_gate = torch.nn.Linear(in_features=config.c_s, out_features=self.total_dim)
+
+        self.linear_output = torch.nn.Linear(in_features=self.total_dim, out_features=config.c_s)
 
     def forward(self, msa_representation: torch.Tensor):
-        pass
+        msa_representation = self.layer_norm_msa(msa_representation)
+
+        # Shape (batch, N_seq, N_res, self.total_dim)
+        Q = self.linear_q(msa_representation)
+        K = self.linear_k(msa_representation)
+        V = self.linear_v(msa_representation)
+
+        # Reshape to (batch, N_seq, N_res, self.num_heads, self.head_dim)
+        Q = Q.reshape((Q.shape[0], Q.shape[1], Q.shape[2], self.num_heads, self.head_dim))
+        K = K.reshape((K.shape[0], K.shape[1], K.shape[2], self.num_heads, self.head_dim))
+        V = V.reshape((V.shape[0], V.shape[1], V.shape[2], self.num_heads, self.head_dim))
+
+        G = self.linear_gate(msa_representation)
+        G = G.reshape((G.shape[0], G.shape[1], G.shape[2], self.num_heads, self.head_dim))
+
+        # Squash values in range 0 to 1 to act as gating mechanism
+        G = torch.nn.functional.sigmoid(G)
+
+        # Shape (batch, N_res, self.num_heads, N_seq, N_seq)
+        scores = torch.einsum('bsihd, btihd -> bihst', Q, K)
+        scores = scores / math.sqrt(self.head_dim)
+
+        attention = torch.nn.functional.softmax(scores, dim=-1)
+
+        # Shape (batch, N_seq, N_res, self.num_heads, self.head_dim)
+        values = torch.einsum('bihst, btihd -> bsihd', attention, V)
+
+        values = G * values
+
+        values = values.reshape((Q.shape[0], Q.shape[1], Q.shape[2], -1))
+
+        output = self.linear_output(values)
+
+        return output
+
 
 class MSATransition(torch.nn.Module):
     def __init__(self, config):
         super().__init__()
-        pass
+        self.n = config.msa_transition_n
+
+        self.layer_norm = torch.nn.LayerNorm(config.c_s)
+
+        self.linear_up = torch.nn.Linear(in_features=config.c_s, out_features=self.n*config.c_s)
+        self.linear_down = torch.nn.Linear(in_features=config.c_s*self.n, out_features=config.c_s)
 
     def forward(self, msa_representation: torch.Tensor):
-        pass
+        msa_representation = self.layer_norm(msa_representation)
+
+        activations = self.linear_up(msa_representation)
+
+        return self.linear_down(torch.nn.functional.relu(activations))
 
 class OuterProductMean(torch.nn.Module):
     def __init__(self, config):
